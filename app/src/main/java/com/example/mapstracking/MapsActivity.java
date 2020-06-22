98ed926f53a9f50a2140518858289fefc75af773
Falentino <falentino.h@gmail.com>
2020-06-22 15:06:11 +0800

    Initialize

---
+++ app/src/main/java/com/example/mapstracking/MapsActivity.java
@@ -0,0 +1,14 @@
+package com.example.mapstracking;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MapsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
