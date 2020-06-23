a2e01dc9ae62d45c9b0d433a58edf4d408f30cdb
Falentino <falentino.h@gmail.com>
2020-06-23 17:10:00 +0800

    Maps Continuous Search Current Location Marked with Marker, Add Retrofit

--- app/src/main/java/com/example/mapstracking/MainActivity.java
+++ app/src/main/java/com/example/mapstracking/MainActivity.java
@@ -9,8 +9,14 @@ import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,38 +29,54 @@ import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
-    GoogleMap googleMap;
+    private LatLng marker1 = new LatLng(1.5050588, 124.8727851);
+    private LatLng marker2 = new LatLng(1.49642737355, 124.878909588);
+    private TextView tvDuration, tvDistance;
     SupportMapFragment supportMapFragment;
-    FusedLocationProviderClient client;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
-        client = LocationServices.getFusedLocationProviderClient(this);
-
-        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            getCurrentLocation();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        final Handler handler = new Handler();
+        final int count = 0;
+        final Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                getCurrentLocation();
+                if(count != 1) {
+                    handler.postDelayed(this, 10000);
+                }
+            }
+        };
+        handler.post(run);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
         } else {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+            getCurrentLocation();
         }
     }
 
     private void getCurrentLocation() {
         @SuppressLint("MissingPermission")
-        Task<Location> task = client.getLastLocation();
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(final Location location) {
-                if (location != null){
+                if (location != null) {
                     supportMapFragment.getMapAsync(new OnMapReadyCallback() {
                         @SuppressLint("MissingPermission")
                         @Override
                         public void onMapReady(GoogleMap googleMap) {
-                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
                             googleMap.setMyLocationEnabled(true);
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                            googleMap.addMarker(new MarkerOptions().position(latLng).title("Current Loc"));
+                            googleMap.addMarker(new MarkerOptions().position(marker1).title("Marker 1"));
+                            googleMap.addMarker(new MarkerOptions().position(marker2).title("Marker 2"));
                         }
                     });
                 }
@@ -64,10 +86,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == 44){
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 getCurrentLocation();
             }
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getCurrentLocation();
     }
 }
---
+++ app/src/main/java/com/example/mapstracking/MapDistance.java
@@ -0,0 +1,16 @@
+package com.example.mapstracking;
+
+import com.google.gson.annotations.SerializedName;
+
+public class MapDistance {
+    public String getDistance() {
+        return distance;
+    }
+
+    public void setDistance(String distance) {
+        this.distance = distance;
+    }
+
+    @SerializedName("distance") private String distance;
+
+}
